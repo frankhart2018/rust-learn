bf84a9d7cc5881ad9021ffe026ff4f97e84fd395
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-02-06 00:37:31 +0530

    Add function ownership

--- ch-4-understanding-ownership/function_ownership/src/main.rs
+++ ch-4-understanding-ownership/function_ownership/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello");
+    
+    takes_ownership(s); // Same as variable ownership, s will be moved to some_string
+
+    // This will throw an error
+    // println!("{}", s);
+
+    let x = 5;
+
+    makes_copy(x); // Same as variable ownership, x will be copied (not moved) to some_integer
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
