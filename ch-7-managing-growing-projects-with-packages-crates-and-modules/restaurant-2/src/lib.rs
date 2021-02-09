bb7a1d039fdb6bf7700bb747196c0ca6030382fc
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-02-09 18:51:31 +0530

    Yet another restaurant example

---
+++ ch-7-managing-growing-projects-with-packages-crates-and-modules/restaurant-2/src/lib.rs
@@ -0,0 +1,10 @@
+// Using a ; instead of {} after module name tells rust to import it from another file
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
