cbb1166229a22c45b07ddc0eefde4aa990166fdf
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-02-13 17:25:46 +0530

    Add recoverable errors, panic, and expect

---
+++ ch-9-error-handling/recoverable-errors/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        }
+    };
+
+    // Alternative to above matches is
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // Unwrap - If Result<T, E> returns Ok then it return the value inside Ok
+    // Otherwise if it returns Error then it will call panic! macro
+    let f = File::open("hello.txt").unwrap();
+
+    // Expect - Same as unwrap but let's us pass own panic message
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
