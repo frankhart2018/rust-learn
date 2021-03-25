ca0e59f3222b7819aefc5c4af3f6e3331bbc4112
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-03-25 20:55:52 +0530

    Add tests

---
+++ ch-11-writing-automated-tests/adder/src/lib.rs
@@ -0,0 +1,127 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    #[allow(unused)]
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+#[allow(unused)]
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)] // This tells Rust to compile and run tests only when we do `cargo run` and not `cargo build`
+// This is mostly used for unit tests because they go in same file as the code
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail!");
+    // }
+
+    use super::*; // Bring the struct and impls to scope
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller)); // If true nothing will happen, if false will call panic!
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2)); // Order does not matter here
+    }
+
+    #[test]
+    #[ignore]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")] // Panic with expected message
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    // Using Result<T, E> in test
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    // We can't use #[should_panic] in tests that return Result<T, E>
+}
+
+// Each thread is run on a new thread
+// When a test fails it panics and the main thread sees that a test thread has died
+// The test is marked as failed
