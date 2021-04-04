30ea02569ad2f4ecd027c8cd543356b175871f58
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-04 18:02:38 +0530

    Add closure capture iterator

---
+++ ch-13-functional-language-features-iterators-and-closured/closure-capture-iterator/src/main.rs
@@ -0,0 +1,62 @@
+fn main() {
+    // 
+    // Using closures that capture their environment
+    //
+
+    // The filter iterator adaptor method takes a closure
+    // that takes each item from the iterator and returns a Boolean
+    // If the closure returns true, the value will be included
+    // in the iterator produced by filter
+    // If the closure returns false, the value won't be included
+    // in the resulting iterator
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    // into_iter() is called to take ownership of shoes
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_my_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
