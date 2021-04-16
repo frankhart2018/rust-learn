d02b33ea7da46b1adf583d8cafe6d85db8d9906d
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-16 20:48:52 +0530

    Add function patterns

---
+++ ch-18-patterns-and-matching/function-pattern/src/main.rs
@@ -0,0 +1,10 @@
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+fn main() {
+    let point = (3, 5);
+    print_coordinates(&point); // Here the first value matches x parameter and the second matches y
+}
+                
