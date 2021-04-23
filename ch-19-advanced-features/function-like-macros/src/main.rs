557f9a0f5a61d4b7603aa7ee6559bd9db6bcbd88
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-23 18:57:02 +0530

    Add function like macros

---
+++ ch-19-advanced-features/function-like-macros/src/main.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    // Function-like macros define macros that look like function calls
+
+    // These take a TokenStream parameter and their definition manipulates
+    // the TokenStream using Rust code as the other two types of procedural
+    // macros do
+
+    // An example is:-
+    // let sql = sql!(SELECT * FROM posts WHERE id=1);
+}
+                
